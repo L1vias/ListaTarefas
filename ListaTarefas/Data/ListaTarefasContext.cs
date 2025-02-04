4b7a7cd16d82d0a2c9d7467a6d5685f4a400879a
Livia Solla <limasenai12@gmail.com>
2025-02-04 16:59:32 -0300

    Inclusão DataBase

---
+++ ListaTarefas/Data/ListaTarefasContext.cs
@@ -0,0 +1,14 @@
+﻿using ListaTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaTarefas.Data
+{
+    public class ListaTarefasContext : DbContext
+    {
+        public ListaTarefasContext(DbContextOptions<ListaTarefasContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<TipoTarefa> TiposTarefa { get; set; }
+    }
+}
